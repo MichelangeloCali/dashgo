008d717c6f1d4ff83a5a4534dc10be67961e029e
michelangelo <michelangelocali@hotmail.com>
2023-02-25 17:13:00 -0300

    refactor: dashboard

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -81,7 +81,7 @@ const Dashboard = () => {
           minChildWidth="320px"
           alignItems="flex-start"
         >
-          <Box p="8" bg="gray.800" borderRadius={8} pb="4">
+          <Box p={['6', '8']} bg="gray.800" borderRadius={8} pb="4">
             <Text fontSize="lg" mb="4">
               Inscritos da semana
             </Text>
@@ -93,7 +93,7 @@ const Dashboard = () => {
               width="100%"
             />
           </Box>
-          <Box p="8" bg="gray.800" borderRadius={8} pb="4">
+          <Box p={['6', '8']} bg="gray.800" borderRadius={8} pb="4">
             <Text fontSize="lg" mb="4">
               Taxa de abertura
             </Text>
