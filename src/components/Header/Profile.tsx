af54d01bdb86b83da725d9a23a652b2c9515f513
michelangelo <michelangelocali@hotmail.com>
2023-02-24 23:03:19 -0300

    refactor: pagination, header, sidebar

---
+++ src/components/Header/Profile.tsx
@@ -0,0 +1,20 @@
+import { Flex, Avatar, Box, Text } from '@chakra-ui/react'
+
+export const Profile = () => {
+  return (
+    <Flex align="center">
+      <Box mr="4" textAlign="right">
+        <Text>Michelangelo Cali</Text>
+        <Text color="gray.300" fontSize="small">
+          michelangelocali@hotmail.com
+        </Text>
+      </Box>
+
+      <Avatar
+        size="md"
+        name="Michelangelo Cali"
+        src="https://avatars.githubusercontent.com/u/90471567?s=400&u=f07f2c9d8cb772408b5396e1a1bc0fe6126a843f&v=4"
+      />
+    </Flex>
+  )
+}
