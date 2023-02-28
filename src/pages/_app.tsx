b87e6991e2c7761d7df23f9af1b9eb998db8e0e2
michelangelo <michelangelocali@hotmail.com>
2023-02-27 22:01:22 -0300

    feat: miragejs

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,13 @@
 import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
+import { makeServer } from '../services/mirage'
 
 import { SidebarDrawerProvider } from '../contexts/SidebarDrawerContext'
 import { theme } from '../styles/theme'
+
+if (process.env.NODE_ENV === 'development') {
+  makeServer()
+}
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
